8c457e602d1747060942e7c800aae577d0ae04ed
alvaro <alvaro@alia.(none)>
2012-02-29 15:56:18 -0800

    Fixed problems with installation

--- install.sh
+++ install.sh
@@ -120,13 +120,13 @@ mkdir cache
 echo
 echo "                                      *** ATTENTION ***"
 echo
-echo "LODSPeaKr needs the web server to have write permissions for lodspeakr/cache/ and lodspeakr/meta/."
+echo "LODSPeaKr needs the web server to have write permissions for $1/cache/ and $1/meta/."
 echo
 echo
 echo "Common ways of doing this:"
-echo " chown -R www-apache lodspeakr/cache lodspeakr/meta (find the name of the apache user in your system)"
-echo " chmod -R g+w lodspeakr/cache lodspeakr/meta (if you have a group in common with the apache user)"
-echo " chmod -R 777 lodspeakr/cache lodspeakr/meta (highly discouraged but useful to test when everything fails. It shouldn't be used in production sites)"
+echo " chown -R www-apache $1/cache $1/meta (find the name of the apache user in your system)"
+echo " chmod -R g+w $1/cache $1/meta (if you have a group in common with the apache user)"
+echo " chmod -R 777 $1/cache $1/meta (highly discouraged but useful to test when everything fails. It shouldn't be used in production sites)"
 echo
 echo "Please give the server write permissions. Otherwise, LODSPeaKr will not work."
 echo
