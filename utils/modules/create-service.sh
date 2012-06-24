5261ae4c9bdbf0f93a7312198f073fc142400704
alvaro <alvaro@alia.(none)>
2012-06-24 15:12:22 -0700

    bootstrap in script generated components

--- utils/modules/create-service.sh
+++ utils/modules/create-service.sh
@@ -3,40 +3,10 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 initToken='services'
+cd $DIR
+modelHtml=`cat service-model.inc`
 
-modelHtml=`cat  <<QUERY
-{%for h in base.header %}
-PREFIX {{h.prefix}}: <{{h.ns}}>
-{%endfor%}
-SELECT DISTINCT ?resource WHERE {
-  GRAPH {%if lodspk.args.arg0 %}<{{lodspk.args.arg0}}>{%else%}?g{%endif%} {
-  	[] a ?resource .
-  }
-}
-LIMIT 10
-QUERY`
-
-viewHtml=`cat  <<VIEW
-<?xml version="1.0" encoding="UTF-8"?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML+RDFa 1.0//EN" "http://www.w3.org/MarkUp/DTD/xhtml-rdfa-1.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" {% for i, ns in lodspk.ns %}xmlns:{{i}}="{{ns}}" {%endfor%}
-      version="XHTML+RDFa 1.0" xml:lang="en">
-  <head>
-    <title>$1</title>
-    <link href="{{lodspk.baseUrl}}css/basic.css" rel="stylesheet" type="text/css" media="screen" />
-    <style type="text/css">
-    </style>
-  </head>
-  <body>
-    <h1>$1</h1>
- 	 <ul>
-    {% for row in models.main %}
-        <li><a href="{{lodspk.baseUrl}}special/instances/{{ row.resource.curie }}">{{row.resource.curie}}</a></li>
-    {% endfor %}
-    </ul>
-  </body>
-</html>
-VIEW`
+viewHtml=`cat  service-view.inc`
 
 #Check models
 mainDir=$DIR/../../components/$initToken/$1/
