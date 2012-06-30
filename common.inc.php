38da9c0952feab95c6641ee0efb8d02b77b845c0
alvaro <alvaro@alia.(none)>
2012-06-30 08:20:33 -0700

    Update version

--- common.inc.php
+++ common.inc.php
@@ -1,5 +1,5 @@
 <?
-$conf['version'] = '20120411';
+$conf['version'] = '20120630';
 $conf['output']['select'] = 'json';
 $conf['output']['describe'] = 'rdf';
 $conf['endpointParams']['config']['show_inline'] = 0;
