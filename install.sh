90dc9bac794c9f4e395f111f5359463f89921995
alvaro <alvaro@alia.(none)>
2012-10-11 11:58:09 -0700

    Fixing install

--- install.sh
+++ install.sh
@@ -117,6 +117,7 @@ echo "RewriteEngine on" > $parent_htaccess
 echo >> $parent_htaccess
 newBase=`echo $basedir|sed -e "s|https\{0,1\}://[^\/]*||g"`
 echo "RewriteBase $newBase" >> $parent_htaccess
+
 cat $root_htaccess >> $parent_htaccess
 echo "RewriteRule ^(.+)\$ $1/index.php?q=\$1 [L]" >> $parent_htaccess
 mkdir cache
